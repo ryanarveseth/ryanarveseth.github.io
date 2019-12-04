7ff7f215ae3430f8b6fae39d86802515e2f1662f
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2019-12-03 18:24:45 -0700

    Update highScores.js

--- Application/highScores.js
+++ Application/highScores.js
@@ -4,6 +4,7 @@ function displayScores() {
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
         var myObj = JSON.parse(this.responseText);
         //if (localStorage.getItem("scores") == null)
         //    var scoresObj = [];
