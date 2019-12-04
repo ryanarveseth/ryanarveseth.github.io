f9161f591f51cdf1896842e6ae03dce0708c2a4c
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2019-12-03 18:28:06 -0700

    Update getHighScores.php

--- Application/getHighScores.php
+++ Application/getHighScores.php
@@ -4,7 +4,10 @@ $file = 'gamedata.txt';
 $gameData = file_get_contents($file);
 
 $myJSON = json_decode($gameData);
-
-echo $myJSON;
-
+if ($myJSON == null) {
+    echo null;
+} 
+else {
+    echo $myJSON;
+}
 ?>
